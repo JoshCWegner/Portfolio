237074e0d3239785ab6d52001ba9d0e19e01273e
Josh123 <wegnerjosh@gmail.com>
2018-10-11 12:08:54 -0500

    new BasketBallGameCommit

--- BasketBallGame/src/main/java/BasketballGame.java
+++ BasketBallGame/src/main/java/BasketballGame.java
@@ -13,6 +13,7 @@ import java.util.Random;
 public class BasketballGame {
 
     public static void main(String[] args) {
+
         Random random = new Random();
         int yourPossession = 10;
         int opponentsPossession = 11;
@@ -20,9 +21,9 @@ public class BasketballGame {
         String rockets = "";
         String warriors = "";
         String celtics = "";
-        String caviliars = "";
+        String lakers = "";
         int possessionsPerTeamForOpponetsTeam = 0;
-        String userInputEnterInOpponentsTeam = "";
+        String userInputEnterInOpponentsTeam;
         int possessionsPerTeamForYourTeam = 0;
         String userInputEnterInTeamToPlayWith;
         int totalPoints = 0;
@@ -30,89 +31,7 @@ public class BasketballGame {
         int yourTeamsTotalPoints = 0;
         int opponetsTeamTotalPoints = 0;
 
-        do {
-            userInputEnterInTeamToPlayWith = io.readString("Enter in a team to play Rockets, Warriors, Celtics, or Caviliars");
-
-            io.print(userInputEnterInOpponentsTeam);
-
-            if (!userInputEnterInTeamToPlayWith.equalsIgnoreCase("rockets")
-                    && !userInputEnterInTeamToPlayWith.equalsIgnoreCase("warriors")
-                    && !userInputEnterInTeamToPlayWith.equalsIgnoreCase("celtics")
-                    && !userInputEnterInTeamToPlayWith.equalsIgnoreCase("caviliars")) {
-                io.print("=== ERROR === Please select a team from above");
-            }
-
-        } while (!userInputEnterInTeamToPlayWith.equalsIgnoreCase("rockets")
-                && !userInputEnterInTeamToPlayWith.equalsIgnoreCase("warriors")
-                && !userInputEnterInTeamToPlayWith.equalsIgnoreCase("celtics")
-                && !userInputEnterInTeamToPlayWith.equalsIgnoreCase("caviliars"));
-
-        do {
-
-            if (userInputEnterInTeamToPlayWith.equalsIgnoreCase("rockets")
-                    || userInputEnterInTeamToPlayWith.equalsIgnoreCase("warriors")
-                    || userInputEnterInTeamToPlayWith.equalsIgnoreCase("celtics")
-                    || userInputEnterInTeamToPlayWith.equalsIgnoreCase("caviliars")) {
-
-                if (userInputEnterInTeamToPlayWith.equalsIgnoreCase("rockets")) {
-                    userInputEnterInOpponentsTeam = io.readString("Enter in a team to play the " + userInputEnterInTeamToPlayWith
-                            + " - Warriors, Celtics, or Caviliars");
-                    if (!userInputEnterInOpponentsTeam.equalsIgnoreCase("warriors")
-                            && !userInputEnterInOpponentsTeam.equalsIgnoreCase("celtics")
-                            && !userInputEnterInOpponentsTeam.equalsIgnoreCase("caviliars")) {
-                        io.print("=== ERROR === Please select a team from above");
-                    }
-                } else if (userInputEnterInTeamToPlayWith.equalsIgnoreCase("warriors")) {
-                    userInputEnterInOpponentsTeam = io.readString("Enter in a team to play the " + userInputEnterInTeamToPlayWith
-                            + " - Rockets, Celtics, or Caviliars");
-                    if (!userInputEnterInOpponentsTeam.equalsIgnoreCase("rockets")
-                            && !userInputEnterInOpponentsTeam.equalsIgnoreCase("celtics")
-                            && !userInputEnterInOpponentsTeam.equalsIgnoreCase("caviliars")) {
-                        io.print("=== ERROR === Please select a team from above");
-                    }
-                } else if (userInputEnterInTeamToPlayWith.equalsIgnoreCase("celtics")) {
-                    userInputEnterInOpponentsTeam = io.readString("Enter in a team to play the " + userInputEnterInTeamToPlayWith
-                            + " - Rockets, Warriors, or Caviliars");
-                    if (!userInputEnterInOpponentsTeam.equalsIgnoreCase("rockets")
-                            && !userInputEnterInOpponentsTeam.equalsIgnoreCase("warriors")
-                            && !userInputEnterInOpponentsTeam.equalsIgnoreCase("caviliars")) {
-                        io.print("=== ERROR === Please select a team from above");
-                    }
-                } else if (userInputEnterInTeamToPlayWith.equalsIgnoreCase("caviliars")) {
-                    userInputEnterInOpponentsTeam = io.readString("Enter in a team to play the " + userInputEnterInTeamToPlayWith
-                            + " - Rockets, Warriors, Celtics");
-                    if (!userInputEnterInOpponentsTeam.equalsIgnoreCase("rockets")
-                            && !userInputEnterInOpponentsTeam.equalsIgnoreCase("warriors")
-                            && !userInputEnterInOpponentsTeam.equalsIgnoreCase("celtics")) {
-                        io.print("=== ERROR === Please select a team from above");
-                    }
-                }
-            }
-        } while (!userInputEnterInOpponentsTeam.equalsIgnoreCase("rockets")
-                && !userInputEnterInOpponentsTeam.equalsIgnoreCase("warriors")
-                && !userInputEnterInOpponentsTeam.equalsIgnoreCase("celtics")
-                && !userInputEnterInOpponentsTeam.equalsIgnoreCase("caviliars"));
-
-        if (userInputEnterInTeamToPlayWith.equalsIgnoreCase("rockets")) {
-            possessionsPerTeamForYourTeam = 101;
-        } else if (userInputEnterInTeamToPlayWith.equalsIgnoreCase("warriors")) {
-            possessionsPerTeamForYourTeam = 102;
-        } else if (userInputEnterInTeamToPlayWith.equalsIgnoreCase("celtics")) {
-            possessionsPerTeamForYourTeam = 99;
-        } else if (userInputEnterInTeamToPlayWith.equalsIgnoreCase("caviliars")) {
-            possessionsPerTeamForYourTeam = 100;
-        }
-
-        if (userInputEnterInOpponentsTeam.equalsIgnoreCase("rockets")) {
-            possessionsPerTeamForOpponetsTeam = 101;
-        } else if (userInputEnterInOpponentsTeam.equalsIgnoreCase("warriors")) {
-            possessionsPerTeamForOpponetsTeam = 102;
-        } else if (userInputEnterInOpponentsTeam.equalsIgnoreCase("celtics")) {
-            possessionsPerTeamForOpponetsTeam = 99;
-        } else if (userInputEnterInOpponentsTeam.equalsIgnoreCase("caviliars")) {
-            possessionsPerTeamForOpponetsTeam = 100;
-        }
-        playGame(random, userInputEnterInTeamToPlayWith, userInputEnterInOpponentsTeam, io, opponetsTeamTotalPoints, yourTeamsTotalPoints);
+        selectTeamsPlayGame(io);
     }
 
     public static void determineWhoWinsTheTipOff(Random random, String userInputEnterInTeamToPlayWith,
@@ -127,7 +46,29 @@ public class BasketballGame {
     }
 
     public static void runPossessionAndPointsForYourTeam(Random random, int possessionsPerTeamForYourTeam,
-            String userInputEnterInTeamToPlayWith, UserInterface io, int yourTeamsTotalPoints) {
+            String userInputEnterInTeamToPlayWith, UserInterface io, int yourTeamsTotalPoints,
+            int possessionsPerTeamForOpponetsTeam, String userInputEnterInOpponentsTeam,
+            int opponetsTeamTotalPoints) {
+
+        if (userInputEnterInTeamToPlayWith.equalsIgnoreCase("rockets")) {
+            possessionsPerTeamForYourTeam = 101;
+        } else if (userInputEnterInTeamToPlayWith.equalsIgnoreCase("warriors")) {
+            possessionsPerTeamForYourTeam = 102;
+        } else if (userInputEnterInTeamToPlayWith.equalsIgnoreCase("celtics")) {
+            possessionsPerTeamForYourTeam = 99;
+        } else if (userInputEnterInTeamToPlayWith.equalsIgnoreCase("lakers")) {
+            possessionsPerTeamForYourTeam = 100;
+        }
+
+        if (userInputEnterInOpponentsTeam.equalsIgnoreCase("rockets")) {
+            possessionsPerTeamForOpponetsTeam = 101;
+        } else if (userInputEnterInOpponentsTeam.equalsIgnoreCase("warriors")) {
+            possessionsPerTeamForOpponetsTeam = 102;
+        } else if (userInputEnterInOpponentsTeam.equalsIgnoreCase("celtics")) {
+            possessionsPerTeamForOpponetsTeam = 99;
+        } else if (userInputEnterInOpponentsTeam.equalsIgnoreCase("lakers")) {
+            possessionsPerTeamForOpponetsTeam = 100;
+        }
 
         for (int i = 0; i < possessionsPerTeamForYourTeam; i++) {
             int toDetermineHowManyPointsYourTeamScoresPerPossession = random.nextInt(4) + 1;
@@ -147,14 +88,7 @@ public class BasketballGame {
                 int pointsPerPossessionYourTeam = 3;
                 yourTeamsTotalPoints = pointsPerPossessionYourTeam + yourTeamsTotalPoints;
             }
-
-            io.print(userInputEnterInTeamToPlayWith + ": " + yourTeamsTotalPoints);
-
         }
-    }
-
-    public static void runPossessionAndPointsForOpponentsTeam(Random random, int possessionsPerTeamForOpponetsTeam,
-            String userInputEnterInOpponentsTeam, UserInterface io, int opponetsTeamTotalPoints) {
         for (int j = 0; j < possessionsPerTeamForOpponetsTeam; j++) {
             int toDetermineHowManyPointsOpponentsTeamScoresPerPossession = random.nextInt(4) + 1;
             if (toDetermineHowManyPointsOpponentsTeamScoresPerPossession == 1) {
@@ -174,39 +108,121 @@ public class BasketballGame {
                 opponetsTeamTotalPoints = pointsPerPossessionOpponentsTeam + opponetsTeamTotalPoints;
             }
         }
+        io.print(userInputEnterInTeamToPlayWith + ": " + yourTeamsTotalPoints);
         io.print(userInputEnterInOpponentsTeam + ": " + opponetsTeamTotalPoints);
 
+        printMessagesForWinningTeam(userInputEnterInTeamToPlayWith, io, userInputEnterInOpponentsTeam, yourTeamsTotalPoints, opponetsTeamTotalPoints);
     }
 
     public static void printMessagesForWinningTeam(String userInputEnterInTeamToPlayWith, UserInterface io,
-            String userInputEnterInOpponentsTeam) {
+            String userInputEnterInOpponentsTeam, int yourTeamsTotalPoints,
+            int opponetsTeamTotalPoints) {
 
-        if (userInputEnterInTeamToPlayWith.equalsIgnoreCase("rockets")
-                || userInputEnterInOpponentsTeam.equalsIgnoreCase("rockets")) {
-            io.print("Chris Paul isn't one of the all-time greats. He's one of the all-time great LOSERS.");
-            io.print("Do it in the playoffs and if you do it in the playoffs...");
-            io.print("Do it in the Conference Finals and if you do it in the Confence Finals...");
-            io.print("Do it in the Finals");
-        } else if (userInputEnterInTeamToPlayWith.equalsIgnoreCase("warriors")
-                || userInputEnterInOpponentsTeam.equalsIgnoreCase("warriors")) {
-            io.print("In my Personal Record Book the Warriors did not win this game");
-        } else if (userInputEnterInTeamToPlayWith.equalsIgnoreCase("caviliars")
-                || userInputEnterInOpponentsTeam.equalsIgnoreCase("caviliars")) {
-            io.print("=== LEBRON ===");
-        } else if (userInputEnterInTeamToPlayWith.equalsIgnoreCase("celtics")
-                || userInputEnterInOpponentsTeam.equalsIgnoreCase("celtics")) {
-            io.print("Kyrie played?!?!");
-            io.print("I thought we was injured");
+        if (yourTeamsTotalPoints > opponetsTeamTotalPoints) {
+            if (userInputEnterInTeamToPlayWith.equalsIgnoreCase("rockets")) {
+                io.print("Chris Paul isn't one of the all-time greats. He's one of the all-time great LOSERS.");
+                io.print("Do it in the playoffs and if you do it in the playoffs...");
+                io.print("Do it in the Conference Finals and if you do it in the Confence Finals...");
+                io.print("Do it in the Finals");
+            } else if (userInputEnterInTeamToPlayWith.equalsIgnoreCase("warriors")) {
+                io.print("In my Personal Record Book the Warriors did not win this game");
+            } else if (userInputEnterInTeamToPlayWith.equalsIgnoreCase("lakers")) {
+                io.print("LeBron");
+            } else if (userInputEnterInTeamToPlayWith.equalsIgnoreCase("celtics")) {
+                io.print("Kyrie played?!?!");
+                io.print("I thought we was injured");
+            }
+        }
+
+        if (opponetsTeamTotalPoints > yourTeamsTotalPoints) {
+            if (userInputEnterInOpponentsTeam.equalsIgnoreCase("rockets")) {
+                io.print("Chris Paul isn't one of the all-time greats. He's one of the all-time great LOSERS.");
+                io.print("Do it in the playoffs and if you do it in the playoffs...");
+                io.print("Do it in the Conference Finals and if you do it in the Confence Finals...");
+                io.print("Do it in the Finals");
+            } else if (userInputEnterInOpponentsTeam.equalsIgnoreCase("warriors")) {
+                io.print("In my Personal Record Book the Warriors did not win this game");
+            } else if (userInputEnterInOpponentsTeam.equalsIgnoreCase("lakers")) {
+                io.print("LeBron");
+            } else if (userInputEnterInOpponentsTeam.equalsIgnoreCase("celtics")) {
+                io.print("Kyrie played?!?!");
+                io.print("I thought we was injured");
+            }
         }
     }
 
-    public static void playGame(Random random, String userInputEnterInTeamToPlayWith, String userInputEnterInOpponentsTeam,
-            UserInterface io, int opponetsTeamTotalPoints, int yourTeamsTotalPoints) {
-        determineWhoWinsTheTipOff(random, userInputEnterInTeamToPlayWith, userInputEnterInOpponentsTeam, io);
-        runPossessionAndPointsForYourTeam(random, 0, userInputEnterInTeamToPlayWith, io, yourTeamsTotalPoints);
-        runPossessionAndPointsForOpponentsTeam(random, 0, userInputEnterInOpponentsTeam, io, opponetsTeamTotalPoints);
+    public static void selectTeamsPlayGame(UserInterface io) {
+        String userInputEnterInOpponentsTeam = "";
+        String userInputEnterInTeamToPlayWith;
+        Random random = new Random();
 
-        io.print(userInputEnterInOpponentsTeam + ": " + opponetsTeamTotalPoints);
-        io.print(userInputEnterInTeamToPlayWith + ": " + yourTeamsTotalPoints);
+        do {
+            userInputEnterInTeamToPlayWith = io.readString("Enter in a team to play Rockets, Warriors, Celtics, or Lakers");
+
+            if (!userInputEnterInTeamToPlayWith.equalsIgnoreCase("rockets")
+                    && !userInputEnterInTeamToPlayWith.equalsIgnoreCase("warriors")
+                    && !userInputEnterInTeamToPlayWith.equalsIgnoreCase("celtics")
+                    && !userInputEnterInTeamToPlayWith.equalsIgnoreCase("lakers")) {
+                io.print("=== ERROR === Please select a team from above");
+            }
+
+        } while (!userInputEnterInTeamToPlayWith.equalsIgnoreCase("rockets")
+                && !userInputEnterInTeamToPlayWith.equalsIgnoreCase("warriors")
+                && !userInputEnterInTeamToPlayWith.equalsIgnoreCase("celtics")
+                && !userInputEnterInTeamToPlayWith.equalsIgnoreCase("lakers"));
+        do {
+
+            if (userInputEnterInTeamToPlayWith.equalsIgnoreCase("rockets")
+                    || userInputEnterInTeamToPlayWith.equalsIgnoreCase("warriors")
+                    || userInputEnterInTeamToPlayWith.equalsIgnoreCase("celtics")
+                    || userInputEnterInTeamToPlayWith.equalsIgnoreCase("lakers")) {
+
+                if (userInputEnterInTeamToPlayWith.equalsIgnoreCase("rockets")) {
+                    userInputEnterInOpponentsTeam = io.readString("Enter in a team to play the " + userInputEnterInTeamToPlayWith
+                            + " - Warriors, Celtics, or Lakers");
+                    if (!userInputEnterInOpponentsTeam.equalsIgnoreCase("warriors")
+                            && !userInputEnterInOpponentsTeam.equalsIgnoreCase("celtics")
+                            && !userInputEnterInOpponentsTeam.equalsIgnoreCase("lakers")) {
+                        io.print("=== ERROR === Please select a team from above");
+                    }
+                } else if (userInputEnterInTeamToPlayWith.equalsIgnoreCase("warriors")) {
+                    userInputEnterInOpponentsTeam = io.readString("Enter in a team to play the " + userInputEnterInTeamToPlayWith
+                            + " - Rockets, Celtics, or Lakers");
+                    if (!userInputEnterInOpponentsTeam.equalsIgnoreCase("rockets")
+                            && !userInputEnterInOpponentsTeam.equalsIgnoreCase("celtics")
+                            && !userInputEnterInOpponentsTeam.equalsIgnoreCase("lakers")) {
+                        io.print("=== ERROR === Please select a team from above");
+                    }
+                } else if (userInputEnterInTeamToPlayWith.equalsIgnoreCase("celtics")) {
+                    userInputEnterInOpponentsTeam = io.readString("Enter in a team to play the " + userInputEnterInTeamToPlayWith
+                            + " - Rockets, Warriors, or Lakers");
+                    if (!userInputEnterInOpponentsTeam.equalsIgnoreCase("rockets")
+                            && !userInputEnterInOpponentsTeam.equalsIgnoreCase("warriors")
+                            && !userInputEnterInOpponentsTeam.equalsIgnoreCase("lakers")) {
+                        io.print("=== ERROR === Please select a team from above");
+                    }
+                } else if (userInputEnterInTeamToPlayWith.equalsIgnoreCase("lakers")) {
+                    userInputEnterInOpponentsTeam = io.readString("Enter in a team to play the " + userInputEnterInTeamToPlayWith
+                            + " - Rockets, Warriors, Celtics");
+                    if (!userInputEnterInOpponentsTeam.equalsIgnoreCase("rockets")
+                            && !userInputEnterInOpponentsTeam.equalsIgnoreCase("warriors")
+                            && !userInputEnterInOpponentsTeam.equalsIgnoreCase("celtics")) {
+                        io.print("=== ERROR === Please select a team from above");
+                    }
+                }
+            }
+        } while (!userInputEnterInOpponentsTeam.equalsIgnoreCase("rockets")
+                && !userInputEnterInOpponentsTeam.equalsIgnoreCase("warriors")
+                && !userInputEnterInOpponentsTeam.equalsIgnoreCase("celtics")
+                && !userInputEnterInOpponentsTeam.equalsIgnoreCase("lakers"));
+
+        userInputEnterInTeamToPlayWith = userInputEnterInTeamToPlayWith.substring(0, 1).toUpperCase()
+                + userInputEnterInTeamToPlayWith.substring(1).toLowerCase();
+
+        userInputEnterInOpponentsTeam = userInputEnterInOpponentsTeam.substring(0, 1).toUpperCase()
+                + userInputEnterInOpponentsTeam.substring(1).toLowerCase();
+
+        determineWhoWinsTheTipOff(random, userInputEnterInTeamToPlayWith, userInputEnterInOpponentsTeam, io);
+        runPossessionAndPointsForYourTeam(random, 0, userInputEnterInTeamToPlayWith, io, 0, 0, userInputEnterInOpponentsTeam, 0);
     }
 }
